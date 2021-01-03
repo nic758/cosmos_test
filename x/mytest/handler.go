07ea02f71f6fcf15398c50936c2d0cc564afb5df
Developer Experience team at Tendermint <hello@tendermint.com>
2021-01-02 19:34:49 -0500

    Initialized with Starport

---
+++ x/mytest/handler.go
@@ -0,0 +1,23 @@
+package mytest
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/my-test/x/mytest/keeper"
+	"github.com/my-test/x/mytest/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// NewHandler ...
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+		switch msg := msg.(type) {
+    // this line is used by starport scaffolding
+		default:
+			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+		}
+	}
+}
